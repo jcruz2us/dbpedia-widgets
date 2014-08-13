f37dad18435664588151817baf847bdc7391d8f9
Jorge Cruz <jorge@tvplus.com>
2014-08-13 11:47:23 -0700

    add new build source files

--- build/app.js
+++ build/app.js
@@ -63,8 +63,7 @@
 							'width="350"' +
 							'height="400"' +
 					'/>';
-				return iframe
-				// return $sce.trustAsHtml(iframe);
+				return iframe;
 			};
 
 			$scope.sanitize = function (str) {
@@ -72,7 +71,14 @@
 			};
 
 			$scope.getWidgetURL = function () {
-				return $location.absUrl() + "&embed=true";
+				var p = $location.protocol();
+				var h = $location.host();
+				var port = $location.port();
+				port = port !== 80 ? ":" + port : "";
+				var url = $location.url();
+				var path = 'embed.html';
+				
+				return p + "://" + h + port + "/" + path + "#" +  url;
 			};
 
 // 			$scope.resource = {
@@ -408,17 +414,69 @@
                     '<p class="comment">{{ resource.comment }}</p>' + 
                 '</div>' +
                 '<div class="grouped-facts-container">' +
-                    '<div class="generic-facts">' +
+                    '<div class="pager">' + 
+                        '<a class="prev" ng-if="hasPrevious()" ng-click="showPrevious()">Prev</a>' + 
+                        '<a class="next" ng-if="hasNext()" ng-click="showNext()">Next</a>' + 
+                    '</div>' +
+                    '<div ng-class="getSectionClasses()" class="generic-facts">' +
                         '<label class="group-header">About</label>' +
                         '<p class="content">' +
                             '{{ resource.comment }}' +
                         '</p>' +
                     '</div>' +
-                    '<dbpedia-grouped-facts facts="groupedFacts" ng-repeat="groupedFacts in resource.facts">' +
+                    '<dbpedia-grouped-facts facts="groupedFacts" ng-repeat="groupedFacts in resource.facts" ng-class="getSectionClasses(groupedFacts.id)">' +
                     '</dbpedia-grouped-facts>' +
                 '</div>',
             scope: {
                 resource: '='       //two-way parent scope binding
+            },
+            link: function (scope) {
+                scope.activeSection = 'About';
+
+                scope.hasPrevious = function () {
+                    return scope.activeSection !== 'About';
+                }
+
+                scope.hasNext = function () {
+                    var hasNext = false;
+                    var lastItem;
+                    if (scope.resource.facts.length) {
+                        lastItem = scope.resource.facts[scope.resource.facts.length -1];
+                        hasNext = scope.activeSection !== lastItem.id;
+                    }
+                    return hasNext;
+                };
+
+                scope.showPrevious = function () {
+                    if (scope.activeIndex === 0) {
+                        scope.activeSection = 'About'
+                    }
+                    else
+                    {
+                        scope.activeIndex--;
+                        scope.activeSection = scope.resource.facts[scope.activeIndex].id;
+                    }
+                }
+
+                scope.showNext = function () {
+                    if (scope.activeSection === 'About') {
+                        scope.activeIndex = 0;
+                        scope.activeSection = scope.resource.facts[0].id;
+                    }
+                    else
+                    {
+                        scope.activeIndex++;
+                        scope.activeSection = scope.resource.facts[scope.activeIndex].id;
+                    }
+                }
+
+                scope.getSectionClasses = function (id) {
+                    id = id || 'About';
+
+                    return {
+                        'active-facts': scope.activeSection === id
+                    };
+                }
             }
         };
     }
